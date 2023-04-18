400effc5ba6e4711b738fddff9ebcb8432eb79f8
Charlotte Sasson <cha@MacBook-Pro-de-Charlotte.local>
2023-04-18 22:41:11 +0200

    various heuristics

--- a_star_algorithm.py
+++ a_star_algorithm.py
@@ -2,60 +2,23 @@ import itertools
 from heuristics import Heuristics
 
 
-class State:
-    """ A plannification problem state """
-    def __init__(self, predicates: list, cost: int = 0, heuristic: float = 0.0):
-        self.predicates = predicates
-        self.cost = cost
-        self.heuristic = heuristic
-        self.parent = None
 
+def plan_cost(plan):
+    """
+    Convert a plan to a cost, handling nonexistent plans.
+    Returns the length of the plan if it exists, otherwise returns positive infinity.
 
-def get_initial_predicate_lists(problem):
-    """ Gives initial state predicates with adjacent predicates put in a separate constant list """
-    constant_predicates, initial_predicates = [], []
+    Args:
+        plan: Plan to convert to a cost.
 
-    for predicate in problem.initial_state.predicates:
-        if predicate[0] in ["adjacent"]:
-            constant_predicates.append(predicate)
-        else:
-            initial_predicates.append(predicate)
-    return initial_predicates, constant_predicates
+    Returns:
+        int: Length of the plan if it exists, otherwise positive infinity.
+    """
+    if plan is None:
+        return float('inf')
+    else:
+        return len(plan)
 
-def state_contains_goal(goals, state, constant_predicates):
-    """ Tests if the state contains all goals """
-    for goal in goals:
-        if goal not in state and goal not in constant_predicates:
-            return False
-    return True
-
-def a_star(domain, problem):
-    """ A* algorithm """
-    closed_list, open_list = [], []
-    objects = problem.initial_state.objects
-    initial_predicates, constant_predicates = get_initial_predicate_lists(problem)
-
-    state = State(predicates=initial_predicates, cost=0, heuristic=0)
-    goals = State(predicates=problem.goals, cost=0, heuristic=0)
-
-    open_list.append(state)
-
-    while len(open_list) > 0:
-        u = min(open_list, key=lambda x: x.heuristic)
-        open_list.remove(u)
-
-        if state_contains_goal(goals.predicates, u.predicates, constant_predicates):
-            return reconstruct_path(u)
-
-        for v in get_neighbors(u.predicates, constant_predicates, domain.actions, objects):
-            v = State(predicates=v, cost=0, heuristic=0)
-            v.parent = u
-            if v not in closed_list and v not in open_list:
-                v.cost = u.cost + 1
-                v.heuristic = v.cost + distance(v, goals)
-                open_list.append(v)
-        closed_list.append(u)
-    raise ValueError("No path found")
 
 def reconstruct_path(node):
     """ Gives the paths to node """
@@ -65,10 +28,14 @@ def reconstruct_path(node):
         path.append(node.predicates)
     return list(reversed(path))
 
-def distance(state, goal):
-    """ Heuristic estimation between state and goal """
-    distance = Heuristics.null_heuristic(state)
-    return distance
+
+def state_contains_goal(goals, state, constant_predicates):
+    """ Tests if the state contains all goals """
+    for goal in goals:
+        if goal not in state and goal not in constant_predicates:
+            return False
+    return True
+
 
 def get_value_predicate(predicate, candidate):
     """ Returns the predicate with arguments replace with candidate's values """
@@ -78,69 +45,171 @@ def get_value_predicate(predicate, candidate):
         candidate_predicate = (predicate[0], '')
     return candidate_predicate
 
-def get_actions(current_state, constant_predicates, all_actions, objects):
-    """ 
-    Gives the list of all possible actions applicable from current_state
-    Arguments:
-    - current_state: the list of current predicates that aren't in constant_predicates
-    - constant_predicates: the list of constant predicates (True all the time)
-    - all_actions: list of all domain actions
-    - objects: list of all problem objects
-    Returns: a list of actions
-    """
-    actions = []
-    for action in all_actions:
-        possible_values = {
-            param[0]: objects[param[1]] for param in zip(action.arg_names, action.types)
-        }
-        for candidate in ( (dict(zip(possible_values, x)) for x in itertools.product(*possible_values.values()) )):
-            action_is_valid = True
-            for precond in action.preconditions.literals:
-                # Negative predicate
-                if precond[0]==-1:
-                    neg_precond = precond[1]
-                    # Applies candidate's values as arguments for the predicate
-                    neg_candidate_precond = get_value_predicate(neg_precond, candidate)
-                    if neg_candidate_precond in current_state or neg_candidate_precond in constant_predicates:
-                        action_is_valid = False
-                        break
-                # Positive predicate
-                else:
-                    candidate_precond = get_value_predicate(precond, candidate)
-                    if candidate_precond not in current_state and candidate_precond not in constant_predicates:
-                        action_is_valid = False
-                        break
-            if action_is_valid:
-                actions.append((action, candidate))
-    return actions
 
-def get_neighbors(current_state, constant_predicates, all_actions, objects):
-    """
-    Gives the list of all reachable states from current_state
-    Arguments:
-    - current_state: the list of current predicates that aren't in constant_predicates
-    - constant_predicates: the list of constant predicates (True all the time)
-    - all_actions: list of all domain actions
-    - objects: list of all problem objects
-    Returns: the list of all successor states of current_state
-    """
-    # get all possible actions
-    actions = get_actions(current_state, constant_predicates, all_actions, objects)
 
-    # compute all successor states
-    states = []
-    for action in actions:
-        state = current_state.copy()
-        action_function, candidate = action
-        for predicate in action_function.effects.literals:
-            if predicate[0]==-1:
-                neg_value_predicate = get_value_predicate(predicate[1], candidate)
-                if neg_value_predicate in state:
-                    state.remove(neg_value_predicate)
-                    state.append((-1, neg_value_predicate))
+class State:
+    """ A plannification problem state """
+    def __init__(self, predicates: list, cost: int = 0, heuristic: float = 0.0):
+        self.predicates = predicates
+        self.cost = cost
+        self.heuristic = heuristic
+        self.parent = None
+
+
+class AStar:
+    def __init__(self, domain, problem) -> None:
+        self.domain = domain
+        self.problem = problem
+        self.constant_predicates = None
+        self.objects = problem.initial_state.objects
+
+
+    def get_initial_predicate_lists(self):
+        """ Gives initial state predicates with adjacent predicates put in a separate constant list """
+        constant_predicates, initial_predicates = [], []
+
+        for predicate in self.problem.initial_state.predicates:
+            if predicate[0] in ["adjacent"]:
+                constant_predicates.append(predicate)
             else:
-                value_predicate = get_value_predicate(predicate, candidate)
-                if value_predicate not in state:
-                    state.append(value_predicate)
-        states.append(state)
-    return states
+                initial_predicates.append(predicate)
+        return initial_predicates, constant_predicates
+
+
+    def get_actions(self, current_state):
+        """ 
+        Gives the list of all possible actions applicable from current_state
+        Arguments:
+        - current_state: the list of current predicates that aren't in constant_predicates
+        Returns: a list of actions
+        """
+        actions = []
+        for action in self.domain.actions:
+            possible_values = {
+                param[0]: self.objects[param[1]] for param in zip(action.arg_names, action.types)
+            }
+            for candidate in ( (dict(zip(possible_values, x)) for x in itertools.product(*possible_values.values()) )):
+                action_is_valid = True
+                for precond in action.preconditions.literals:
+                    # Negative predicate
+                    if precond[0]==-1:
+                        neg_precond = precond[1]
+                        # Applies candidate's values as arguments for the predicate
+                        neg_candidate_precond = get_value_predicate(neg_precond, candidate)
+                        if neg_candidate_precond in current_state or neg_candidate_precond in self.constant_predicates:
+                            action_is_valid = False
+                            break
+                    # Positive predicate
+                    else:
+                        candidate_precond = get_value_predicate(precond, candidate)
+                        if candidate_precond not in current_state and candidate_precond not in self.constant_predicates:
+                            action_is_valid = False
+                            break
+                if action_is_valid:
+                    actions.append((action, candidate))
+        return actions
+
+
+    def get_neighbors(self, current_state):
+        """
+        Gives the list of all reachable states from current_state
+        Arguments:
+        - current_state: the list of current predicates that aren't in constant_predicates
+
+        Returns: the list of all successor states of current_state
+        """
+        # get all possible actions
+        actions = self.get_actions(current_state)
+
+        # compute all successor states
+        states = []
+        for action in actions:
+            state = current_state.copy()
+            action_function, candidate = action
+            for predicate in action_function.effects.literals:
+                if predicate[0]==-1:
+                    neg_value_predicate = get_value_predicate(predicate[1], candidate)
+                    if neg_value_predicate in state:
+                        state.remove(neg_value_predicate)
+                        state.append((-1, neg_value_predicate))
+                else:
+                    value_predicate = get_value_predicate(predicate, candidate)
+                    if value_predicate not in state:
+                        state.append(value_predicate)
+            states.append(state)
+        return states
+
+
+    def a_star_algorithm(self, heuristic_name):
+        """ A* algorithm """
+        closed_list, open_list = [], []
+        initial_predicates, self.constant_predicates = self.get_initial_predicate_lists()
+
+        state = State(predicates=initial_predicates, cost=0, heuristic=0)
+        goals = State(predicates=self.problem.goals, cost=0, heuristic=0)
+
+        open_list.append(state)
+
+        while len(open_list) > 0:
+            u = min(open_list, key=lambda x: x.heuristic)
+            open_list.remove(u)
+
+            if state_contains_goal(goals.predicates, u.predicates, self.constant_predicates):
+                return reconstruct_path(u)
+
+            for v in self.get_neighbors(u.predicates):
+                v = State(predicates=v, cost=0, heuristic=0)
+                v.parent = u
+                if v not in closed_list and v not in open_list:
+                    v.cost = u.cost + 1
+                    v.heuristic = v.cost + self.distance(v.predicates, goals.predicates, heuristic_name)
+                    open_list.append(v)
+            closed_list.append(u)
+        raise ValueError("No path found")
+
+
+    def distance(self, state, goal, heuristic_name):
+        """ Heuristic estimation between state and goal """
+        if heuristic_name == 'null_heuristic':
+            distance = 0
+
+        # if heuristic_name == 'monotone':
+        #     monotone_plan = a_star(domain, problem, heuristic_name='null_heuristic', state=state, monotone=True)
+        #     distance = plan_cost(monotone_plan)
+
+        if heuristic_name=="hamming_distance":
+            distance = sum(s != g for s, g in zip(state, goal))
+
+        if heuristic_name=="predicates_difference":
+            distance = abs(len(state) - len(goal))
+
+        if heuristic_name=="sum_of_subgoals":
+            subgoals = [g for g in goal if g not in state]
+            distance = 0
+            while subgoals:
+                best_distance = float('inf')
+                best_successor = None
+                for successor in self.get_neighbors(state):
+                    successor_distance = sum(g not in successor for g in subgoals)
+                    if successor_distance < best_distance:
+                        best_distance = successor_distance
+                        best_successor = successor
+                distance += 1
+                state = best_successor
+                subgoals = [g for g in subgoals if g not in state]
+
+        if heuristic_name=="max_proposition":
+            max_propositions = 0
+            for prop in goal:
+                visited = [set()]
+                queue = [(state, 0)]
+                while queue:
+                    current, distance = queue.pop(0)
+                    if prop in current:
+                        max_propositions = max(max_propositions, distance)
+                        break
+                    visited.append(current)
+                    for successor in self.get_neighbors(current):
+                        if successor not in visited:
+                            queue.append((successor, distance + 1))
+        return distance
--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 from time import time
 
 from parser_files.pddlparser import PDDLParser
-from a_star_algorithm import a_star
+from a_star_algorithm import AStar
 
 parser = PDDLParser()
-domain = parser.parse('pddl_files/groupe1/domain.pddl')
-problem = parser.parse('pddl_files/groupe1/problem0.pddl')
+domain = parser.parse('pddl_files/groupe2/domain.pddl')
+problem = parser.parse('pddl_files/groupe2/problem2.pddl')
+
+a_star = AStar(domain=domain, problem=problem)
 
 start = time()
-plan = a_star(domain, problem)
+plan = a_star.a_star_algorithm(heuristic_name="null_heuristic")
 end = time()
+total_time = round(end - start, 2)
 
-print(f"\nComputed in {round(end - start, 2)}s\n")
+minutes = total_time // 60
+seconds = total_time % 60
+
+
+print(f"\nComputed in {int(minutes)} min {seconds}s\n")
 print("-- Plan --")
 for i, state in enumerate(plan):
     for pred in state:
         if pred[0]=="at":
             print(f"Step {i}:\n", pred)
+
+
+## Groupe 2 ##
+
+# Problem 1 224.8s
+# 
+# -- Plan --
+# Step 0:
+#  ('at', 'pos82')
+# Step 1:
+#  ('at', 'pos81')
+# Step 2:
+#  ('at', 'pos71')
+# Step 3:
+#  ('at', 'pos61')
+# Step 4:
+#  ('at', 'pos61')
+# Step 5:
+#  ('at', 'pos51')
+# Step 6:
+#  ('at', 'pos41')
+# Step 7:
+#  ('at', 'pos31')
+# Step 8:
+#  ('at', 'pos21')
+# Step 9:
+#  ('at', 'pos11')
+
+
